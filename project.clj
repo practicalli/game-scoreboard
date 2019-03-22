6e3ffbb86d398785c4278678cc1e3027292668e2
John Stevenson <john@jr0cket.co.uk>
2019-03-22 15:14:57 +0000

    Procfile to deploy app to heroku
    Added a Profile to tell Heroku how to run the application once it has been
    deployed.
    
    Also added `:min-lein-version` to tell Heroku which version of Leiningen to use.

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
- (defproject game-scoreboard "0.1.0-SNAPSHOT"
-   :description "An Game Scoreboard API"
-   :dependencies [[org.clojure/clojure "1.10.0"]
-                  [metosin/compojure-api "1.1.11"]]
-   :ring {:handler game-scoreboard.handler/app}
-   :uberjar-name "server.jar"
-   :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
+(defproject game-scoreboard "0.1.0-SNAPSHOT"
+  :description "An Game Scoreboard API"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [metosin/compojure-api "1.1.11"]]
+  :ring {:handler game-scoreboard.handler/app}
+  :min-lein-version "2.9.1"
+  :uberjar-name "server.jar"
+  :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
                                   [cheshire "5.5.0"]
                                   [ring/ring-mock "0.3.0"]]
-                   :plugins [[lein-ring "0.12.0"]]}})
+                   :plugins      [[lein-ring "0.12.0"]]}})
