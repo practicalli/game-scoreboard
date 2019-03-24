3d48d66be3f28148c7e7bcb5c107aab3b2d8c9d3
John Stevenson <john@jr0cket.co.uk>
2019-03-24 12:29:43 +0000

    Test namespace - comment old test, document sections
    Commented out the `deftest` function that was generated by the compojure-api
    template.
    
    Documented helper function and API test sections to make the namespace easier to
    follow.

--- test/game_scoreboard/core_test.clj
+++ test/game_scoreboard/core_test.clj
@@ -4,13 +4,26 @@
             [game-scoreboard.handler :refer :all]
             [ring.mock.request :as mock]))
 
+;; Helper functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn parse-body [body]
   (cheshire/parse-string (slurp body) true))
 
-(deftest a-test
 
-  (testing "Test GET request to /hello?name={a-name} returns expected response"
-    (let [response (app (-> (mock/request :get  "/api/plus?x=1&y=2")))
-          body     (parse-body (:body response))]
+;; API focused tests
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+#_(deftest a-test
+
+    (testing "Test GET request to /hello?name={a-name} returns expected response"
+
+      (let [response (app (-> (mock/request :get  "/game/plus?x=1&y=2")))
+            body     (parse-body (:body response))]
+
+        (is (= (:status response) 200))
+        (is (= (:result body) 3)))))
+
       (is (= (:status response) 200))
       (is (= (:result body) 3)))))
