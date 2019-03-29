384dc2693e3e3b3bd9994729d05d664f5856983a
John Stevenson <john@jr0cket.co.uk>
2019-03-29 13:34:13 +0000

    Fix: Leiningen min version 2.8.3
    Heroku only seems to support Leiningen version 2.8.3, it certainly fails to download
    version 2.9.1.
    
    Also, it seems there is a conflict between the ring library used by
    compojure-api and Leiningen 2.9.1.  The application code fails to compile due to
    a spec error.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [metosin/compojure-api "1.1.11"]]
   :ring {:handler game-scoreboard.handler/app}
-  :min-lein-version "2.9.1"
+  :min-lein-version "2.8.3"
   :uberjar-name "server.jar"
   :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
                                   [cheshire "5.5.0"]
